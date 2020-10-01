bb9cf1d53de54b3f924036f28bc4b0faa0178647
Jun Potisanmetee <54251567+iapisak@users.noreply.github.com>
2020-10-01 16:30:40 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbUrl = process.env.MONGODB_URI;
+const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/event-app';
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
