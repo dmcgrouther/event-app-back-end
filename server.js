a6393e1eb6fb762dc495c62d36b2e908672bf185
david-mcgrouther <dbmcgrouther@gmail.com>
2020-10-12 15:05:29 -0700

    added cors file. backend and front end are synced up!! only got the eventname to get added in from front end. just eventname form is there, as well as backend eventname in model

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const routes = require('./routes');
+
+const corsOptions = {
+    origin: [`http://localhost:3000`],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions))
 
 //bodyparser
 app.use(bodyParser.urlencoded({extended: true}));
