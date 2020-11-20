45f7cafd2b032333118169e48a744a7d776a3b50
david-mcgrouther <dbmcgrouther@gmail.com>
2020-11-20 15:42:39 -0800

    making commit from juns code so i can push to master

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-router.post('auth/register', ctrl.auth.register);
-router.post('auth/login', ctrl.auth.login);
-router.delete('auth/logout', ctrl.auth.logout);
+router.post('/register', ctrl.auth.register);
+router.post('/login', ctrl.auth.login);
+router.delete('/logout', ctrl.auth.logout);
 
 module.exports = router;
--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-router.post('events/', ctrl.events.createEvent);
+router.post('/', ctrl.events.createEvent);
 router.get('/', ctrl.events.showAllEvents);
 router.get('/:eventId', ctrl.events.showOneEvent);
 router.put('/:eventId', ctrl.events.editOneEvent);
