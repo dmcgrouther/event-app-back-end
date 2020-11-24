74ed1f113e217db723601e60d064edf621543eeb
david-mcgrouther <dbmcgrouther@gmail.com>
2020-11-24 14:38:57 -0800

    edit user controller and route working

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,26 @@ const showAUser = (req, res) => {
     });
 };
 
+const editCurrentUser = (req, res) => {
+    db.User.findByIdAndUpdate(
+        req.params.userId,
+        req.body,
+        {new: true}, (error, updatedUser) => {
+            if (error) return res.status(500).json({
+                status: 500,
+                error: [{message: 'Something went wrong! Please try again'}],
+            });
+
+            res.json({
+                status: 200,
+                count: 1,
+                data: updatedUser,
+                requestedAt: new Date().toLocaleString()
+            })
+        })
+}
+
 module.exports = {
-    showAUser
+    showAUser,
+    editCurrentUser
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-router.get('/:userId', ctrl.users.showAUser)
+router.get('/:userId', ctrl.users.showAUser);
+router.put('/:userId', ctrl.users.editCurrentUser);
 
 module.exports = router;
