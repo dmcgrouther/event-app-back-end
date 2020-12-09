5e33a9ccc2f8e6d25bce6beeb50b748070ff8d5f
david-mcgrouther <dbmcgrouther@gmail.com>
2020-12-09 08:48:24 -0800

    removed davids experimental code that didint work. commented out delete current user where it just deletes user. juns experimental code in there, uncomplete

--- controllers/users.js
+++ controllers/users.js
@@ -32,49 +32,52 @@ const editCurrentUser = (req, res) => {
         })
 }
 
-const deleteCurrentUser = (req, res) => {
-    db.User.findByIdAndDelete(
-      req.params.userId, (err, deleteUser) =>{
-        if (err)  return res.status(500).json({
-          status: 500,
-          error: [{message: 'Something went wrong! Please try again'}],
-        });
-        res.json({
-          status:200,
-          count: 1,
-          data: deleteUser,
-          requestedAt: new Date().toLocaleString(),
-        })
-      })
-  }
-
 // const deleteCurrentUser = (req, res) => {
-//     db.User.findById(req.params.userId, (err, foundUser) => {
-//         if (err) return res.status(500).json({
-//             status: 500,
-//             message: err
+//     db.User.findByIdAndDelete(
+//       req.params.userId, (err, deleteUser) =>{
+//         if (err)  return res.status(500).json({
+//           status: 500,
+//           error: [{message: 'Something went wrong! Please try again'}],
 //         });
-//         return ( res.status(200).json({
-//             status: 200,
-//             data: foundUser,
-//         }), 
-//         foundUser.eventsUserIsHosting.forEach(eventUserIsHosting => {
-//             console.log(eventUserIsHosting)
-//             db.Event.findById(eventUserIsHosting, (err, foundEvent) => {
-//                 if (err) return res.status(500).json({
-//                     status: 500,
-//                     message: err
-//                 });
-//                 return res.status(201).json({
-//                     status: 201,
-//                     count: 1,
-//                     data: foundEvent,
-//                     requestedAt: new Date().toLocaleString(),
-//                 }) 
-//             })
-//         }))
-//     })
-// }
+//         res.json({
+//           status:200,
+//           count: 1,
+//           data: deleteUser,
+//           requestedAt: new Date().toLocaleString(),
+//         })
+//       })
+//   }
+
+const deleteCurrentUser = async (req, res) => {
+    let userId = req.params.userId
+    let eventsAttending = []
+    try {
+        // const deleteUser = await db.User.findByIdAndDelete(userId)
+        const deleteUser = await db.User.findById(userId)
+        const eventsFound = await db.Event.find()
+        console.log(eventsFound)
+        console.log(userId)
+    
+        eventsFound.forEach(eventFound => {
+
+            const foundEvent = eventFound.nonHostUsers.findById(userId)
+            console.log(foundEvent)
+            // eventFound.nonHostUsers.findById(userId, (error, eventAsAttendee) => {
+            //     if (error) {
+            //         return res.status(500).json({ error: "Cloud not find this user"})
+            //     } else {
+
+            //         console.log(eventAsAttendee)
+            //         eventsAttending.push(eventAsAttendee)
+            //     }
+            // })
+        })
+        
+        return res.status(200).json({ status: 200, data: deleteUser, delete: eventsAttending })
+    } catch {
+        return res.status(500).json({ error: "Cloud not find this user"})
+    }
+}
 
 module.exports = {
     showAUser,
